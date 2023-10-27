32780162b1f885e996572a26ebd113de1b4fe219
LiabilityJones <131956362+LiabilityJones@users.noreply.github.com>
2023-10-27 10:38:38 +0200

    Update and rename mandarin_helpers to mandarin_helpers.py

--- mandarin_helpers.py
+++ mandarin_helpers.py
@@ -9,11 +9,11 @@ def get_current_time():
     split_time = replace_time.split(" ")
     return str(split_time[0]), int(split_time[1]), int(split_time[2])
 
-# Takes variable of audio file for English, determines length, and returns audio clip based on time-turned-index
+# Takes variable of audio file for Mandarin, determines length, and returns audio clip based on time-turned-index
 def slice_silence_mandarin(audio_file):
     indication, hour, minute = get_current_time()
     audio = AudioSegment.from_wav(audio_file)
-    split_audio = silence.split_on_silence(audio, silence_thresh=-45, keep_silence=800)
+    split_audio = silence.split_on_silence(audio, silence_thresh=-50, keep_silence=800)
     if len(split_audio) == 12:
         part = split_audio[hour - 1]
         part.export("sound_1.wav", format="wav")
@@ -35,7 +35,7 @@ def slice_silence_mandarin(audio_file):
             part.export("sound_3.wav", format="wav")
             return "sound_3.wav"
 
-# Takes 3 audio files, and constructs them based on English conventions
+# Takes 3 audio files, and constructs them based on Mandarin conventions
 def combine_audio_mandarin(audio_3, audio_1, audio_2):
     sound1 = AudioSegment.from_file(audio_1, format="wav")
     sound2 = AudioSegment.from_file(audio_2, format="wav")
