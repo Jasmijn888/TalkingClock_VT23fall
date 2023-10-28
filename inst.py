d3e8e59c3b97406dfd6356d9deb148e632738859
Jasmijn888 <144037999+Jasmijn888@users.noreply.github.com>
2023-10-28 11:34:25 +0200

    Update inst.py
    change comments

--- inst.py
+++ inst.py
@@ -58,21 +58,21 @@ engine = init()
 
 
 
-# 全局变量
+# time zone by default
 selected_timezone = "Asia/Shanghai"
 
-# 函数：选择时区
+# function: select time zone
 def select_timezone(value):
     global selected_timezone
     selected_timezone = value
     update_time()
 
-# 函数：播放当前时间
+# function: read the current time
 def read_time():
     current_time = get_current_time()
     play_audio(f"{current_time}_{selected_timezone}.wav")
 
-# 函数：设置闹钟
+# function: set alarm
 def set_alarm():
     alarm_time = simpledialog.askstring("Set Alarm", "Enter the alarm time (HH:MM):")
     if alarm_time is not None:
@@ -82,7 +82,7 @@ def set_alarm():
         except ValueError:
             messagebox.showerror("Invalid Time", "Please enter a valid time (HH:MM)")
 
-# 函数：获取当前时间
+# function: get the current time (time zone)
 def get_current_time():
     now = datetime.now(pytz.timezone(selected_timezone))
     current_time = now.strftime("%H:%M:%S")
@@ -90,7 +90,7 @@ def get_current_time():
 
 ##########################
 
-# 函数：播放音频
+# function: play audio file
 def play_audio(audio_file):
     pygame.mixer.music.load(audio_file)
     pygame.mixer.music.play()
@@ -106,29 +106,29 @@ frame.pack()  # This positions the frame within the 'master' window
 
 
 
-# 创建下拉框来选择时区
+# drop box for time zone selection
 timezones = {"China (Shanghai)": "Asia/Shanghai", "Germany (Berlin)": "Europe/Berlin", "USA (New York)": "America/New_York"}
 timezone_var = tk.StringVar()
 timezone_var.set(selected_timezone)
 timezone_menu = tk.OptionMenu(frame, timezone_var, *timezones, command=lambda value: select_timezone(timezones[value]))
 timezone_menu.grid(row=1, column=0)
 
-# 创建按钮来读取当前时间
+# button for read the time
 read_time_button = tk.Button(frame, text="Read the Time", command=read_time)
 read_time_button.grid(row=1, column=1)
 
-# 创建按钮来设置闹钟
+# button for alarm setting
 set_alarm_button = tk.Button(frame, text="Set Alarm", command=set_alarm)
 set_alarm_button.grid(row=1, column=2)
 
 style_button = tk.Button(frame, text='Switch Style', command=switch_clock_style)
 style_button.grid(row=0, column=0, columnspan=3)
 
-# 创建标签来显示当前时间
+# time label for current time -digital
 #time_label = tk.Label(frame, text=get_current_time(), font=("Helvetica", 24))
 #time_label.grid(row=2, column=0, columnspan=3)
 
-# 启动定时器来更新时间
+# update time
 def update_time():
     current_time = get_current_time()
     time_label.config(text=current_time)
@@ -138,7 +138,7 @@ def update_clock_pointer():
     now = datetime.now(pytz.timezone(selected_timezone))
     current_time = now.time()
 
-    # 与之前代码保持一致，计算角度
+    # pointer angel
     hours = current_time.hour
     minutes = current_time.minute
     seconds = current_time.second
@@ -176,6 +176,6 @@ def update_clock_pointer():
 update_time()
 update_clock_pointer()
 
-# 运行主程序
+# mainloop
 master.mainloop()
 
